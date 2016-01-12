8cade12a178190b6aa17c3d422e8422592959ec9
sansang <sansang072012@gmail.com>
2016-01-12 12:39:42 +0800

    test

---
+++ app.js
@@ -0,0 +1,64 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+
+var usernames = {};
+
+var rooms = ['room1','room2','room3'];
+
+io.sockets.on('connection', function (socket) {
+
+	socket.on('adduser', function(username){
+		socket.username = username;
+		socket.room = 'room1';
+		usernames[username] = username;
+		socket.join('room1');
+		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.emit('updaterooms', rooms, 'room1');
+        io.to('room1').emit('updateusers', usernames);
+              
+	});
+
+
+	socket.on('sendchat', function (data) {
+		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+        
+	});
+
+	socket.on('switchRoom', function(newroom){
+		socket.leave(socket.room);
+		socket.join(newroom);
+		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+		socket.room = newroom;
+		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+		socket.emit('updaterooms', rooms, newroom);
+        io.to(newroom).emit('updateusers', usernames);
+              
+	});
+
+	socket.on('disconnect', function(){
+		delete usernames[socket.username];
+		io.sockets.emit('updateusers', usernames);
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		socket.leave(socket.room);
+              
+	});
+});
